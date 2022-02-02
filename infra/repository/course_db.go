0d5b41819fbabe9a532684da46037f84e90d0195
contecloud <carlo.conte@gmail.com>
2022-02-01 23:08:37 -0300

    Inicio do intensivão

---
+++ infra/repository/course_db.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/contecloud/intensivao-golang/entity"
+)
+
+type CourseMySQLRepository struct {
+	Db *sql.DB
+}
+
+func (c CourseMySQLRepository) Insert(course entity.Course) error {
+	stmt, err := c.Db.Prepare(`insert into courses(id, name, description, status) values (?,?,?,?,)`)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(
+		course.ID,
+		course.Name,
+		course.Description,
+		course.Status,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
